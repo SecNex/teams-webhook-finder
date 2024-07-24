57a4f77dc958eb409afd473086618e64b3589473
Björn Benouarets <bbenouarets@proton.me>
2024-07-24 23:16:00 +0200

    feat: Remove scope.go - not required

--- api/teams/scope.go
+++
@@ -1,7 +0,0 @@
-package teams
-
-const SCOPE_TEAM_READBASIC_ALL = "Team.ReadBasic.All"
-const SCOPE_TEAMSAPPINSTALLATION_READ_ALL = "TeamsAppInstallation.Read.All"
-const SCOPE_TEAMSAPPINSTALLATION_READFORUSER_ALL = "TeamsAppInstallation.ReadForUser.All"
-const SCOPE_TEAMSAPPINSTALLATION_READFORTEAM_ALL = "TeamsAppInstallation.ReadForTeam.All"
-const SCOPE_APPCATALOG_READ_ALL = "AppCatalog.ReadWrite.All"
